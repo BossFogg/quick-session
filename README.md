7f458a45c69164c66d34b4f9163044e62684752e
Richard Bond <richardlewis.bond@gmail.com>
2019-06-29 23:39:27 -0700

    imported existing code and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # quick-session
 A simple, quick-set-up session manager
+
+## Installation
+Install package with NPM:
+`npm i quick-session`
+
+Initialize quick-session in Node.js:
+`const qSession = require('quick-session');
+qSession.salt = "StringOfYourChoice";`
+
+Quick Session is ready to use! But there are some optional parameters you can set:
+`qSession.encryptScheme = "aes128"; //default "aes-123-cbc"
+qSession.expireTime = 2000000; //Time in milliseconds till a session expires. Default 1800000`
---
+++ session.js
@@ -0,0 +1,75 @@
+const crypto = require("crypto");
+
+let sessionManager = {};
+
+//Set this variable after importing the module
+sessionManager.salt;
+
+//Use defaults or set values after importing
+sessionManager.encryptScheme = "aes-256-cbc";
+sessionManager.expireTime = 1800000;
+
+sessionManager.sessionList = [];
+
+sessionManager.findSessionById = function(id) {
+	let foundSession;
+	this.sessionList.forEach(function(session) {
+		if (sessionManager.expired(session)) { sessionManager.cleanup(session); }
+		else if (session.id == id) { 
+			session.created = new Date().getTime();
+			foundSession = session; 
+		}
+	})
+	return foundSession;
+}
+
+sessionManager.findSessionByToken = function(token) {
+	let foundSession;
+	this.sessionList.forEach(function(session) {
+		if (sessionManager.expired(session)) { sessionManager.cleanup(session); }
+		else if (session.token == token) { 
+			session.created = new Date().getTime();
+			foundSession = session; 
+		}
+	})
+	return foundSession;
+}
+
+sessionManager.newSession = function(id) {
+	let newSession = {
+		id: id,
+		created: new Date().getTime(),
+		token: sessionManager.createToken(id, this.created)
+	}
+	if (newSession.token) {
+		this.sessionList.push(newSession);
+		return newSession;
+	}
+	else { return null; }
+}
+
+sessionManager.expired = function(session) {
+	let now = new Date().getTime();
+	if ((now - session.created) > this.expireTime) { return true; }
+	return false;
+}
+
+sessionManager.createToken = function(id, created) {
+	if (!this.salt || !this.encryptScheme) { 
+		console.log("Error: missing salt and/or encrypt scheme!");
+		return null;
+	}
+	else {
+		let cipher = crypto.createCipher(this.encryptScheme, this.salt);
+		let token = cipher.update(id + created, "utf8", "hex");
+		token += cipher.final("hex");
+		return token;
+	}
+}
+
+sessionManager.cleanup = function(session) {
+	let index = this.sessionList.indexOf(session);
+	this.sessionList.splice(index, 1);
+}
+
+module.exports = sessionManager;
